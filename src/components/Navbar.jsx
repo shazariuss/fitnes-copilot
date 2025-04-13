42f5aee88c891fa9154a600373964357ea38765e
shazariuss <shazariuss@gmail.com>
2025-04-13 07:43:38 +0500

    admin button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,6 +36,12 @@ function Navbar() {
                             >
                                 Dashboard
                             </Link>
+                            <Link
+                                to="/admin"
+                                className="border-transparent text-gray-500 hover:border-primary-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                            >
+                                Admin
+                            </Link>
                         </div>
                     </div>
 
